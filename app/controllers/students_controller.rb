48f457da6b4d64f878e898c1b430f0f58b67505e
jb5595 <jacobberman1995@gmail.com>
2018-09-22 08:48:32 -0400

    completed lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,23 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
+  end
+
+  def new
   end
 
   def show
     @student = Student.find(params[:id])
   end
+
+  def create
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    @student = Student.create(first_name: first_name, last_name: last_name)
+    redirect_to @student
+  end
+
+
 
 end
