9345b76b6ddabc9c643081631f978e9cfbfdc595
Trevor Jameson <github email address>
2018-09-24 10:26:00 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
 class StudentsController < ApplicationController
-  
+
   def index
     @students = Student.all
   end
 
   def show
     @student = Student.find(params[:id])
+  end
+
+  def new_student
+    render "new"
+  end
+
+  def create
+    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+  end
+
+private
+
+  def fullname
+    @full_name = "#{@student.first_name} #{@student.last_name}"
   end
 
 end
