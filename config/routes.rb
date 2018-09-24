d15b221ee25fed23c034fe8080c68862058a142c
github username <github email address>
2018-09-24 16:06:19 -0400

    rails form tag lab.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :students, only: [:index, :new, :create]
   get '/students/:id', to: 'students#show', as: 'student'
+
+  # resources :school_classes, only: [:index, :new, :create, :edit, :update]
+  # get '/school_classes/:id', to: 'school_classes#show', as: 'school_class'
+
+  resources :school_classes, except: [:destroy, :index]
+
 end
---
+++ spec/factories/school_classes.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :school_class do
+    title { "MyString" }
+    room_number { "MyString" }
+    integer { "MyString" }
+  end
+end
